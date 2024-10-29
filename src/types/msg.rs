23257f9a52fc5cadb194bdf0adca67a05442d75a
srdtrk <srdtrk@hotmail.com>
2024-10-29 15:02:44 +0800

    imp: added msg type

--- src/types/msg.rs
+++ src/types/msg.rs
@@ -3,6 +3,7 @@
 //! This module defines the messages that this contract receives.
 
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Binary, Empty};
 
 /// The message to instantiate the contract.
 #[cw_serde]
@@ -15,4 +16,15 @@ pub enum ExecuteMsg {}
 /// The query messages supported by the contract.
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    /// Verifies an SP1 proof.
+    #[returns(Empty)]
+    VerifyProof {
+        /// The proof to verify.
+        proof: Binary,
+        /// The public values to verify the proof against.
+        public_values: Binary,
+        /// The verification key of the sp1 program.
+        vk: Binary,
+    },
+}
